6799ce86154e72e891472d3eb0ce5f907cf090d1
khadija <khadijanisar949@gmail.com>
2023-08-11 18:08:35 +0500

    css featur

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     GOOGLE_CLIENT_ID: "256014518823-pgdm1bna90vojtjghthh8otiu154ihid.apps.googleusercontent.com",
     GOOGLE_SECRET: "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",
     GITHUB_ID:"2b6204c204dd01509d7c",
-    GITHUB_SECRET:"fc48e03220a60075872630b730de15f487a085e0",
+    GITHUB_SECRET:"a982995a9c1fc1a1562a0f9723f40e829e22f27a",
   },
 
 };
