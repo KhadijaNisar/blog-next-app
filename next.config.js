2d17ff9e035a15da6906621ea13bc35b48b7fbb1
khadija <khadijanisar949@gmail.com>
2023-08-11 18:11:06 +0500

    css featu

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
     GOOGLE_CLIENT_ID: "256014518823-pgdm1bna90vojtjghthh8otiu154ihid.apps.googleusercontent.com",
     GOOGLE_SECRET: "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",
     GITHUB_ID:"2b6204c204dd01509d7c",
-    GITHUB_SECRET:"a982995a9c1fc1a1562a0f9723f40e829e22f27a",
+    GITHUB_SECRET:"eb9cadb0228097a9caf5c77e020b8216effa0699",
   },
 
 };
